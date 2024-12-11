6b1cf52e139d8a1e01e071c6ac7a27094cb5ae5a
Will Oestreich <woestreich.research@gmail.com>
2024-12-11 13:34:27 -0800

    updated maps

--- vis.R
+++ vis.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 library(patchwork)
+library(fst)
 
 pie <- read.csv("outputs/obis_zone_summary_land.csv")
 
-obis_clean <- read.csv("outputs/obis_clean.csv")
+obis_clean <- read_fst("outputs/obis_clean.fst")
 
 zone_colors <- c("deep (benthic)" = "black", "deep (midwater)" = "#1f78b4",
                  "coastal or shallow" = "#a6cee3", "land" = "#33a02c")
@@ -41,7 +42,7 @@ p <- obis_clean |>
   theme_bw() +
   coord_flip() +
   scale_x_reverse() +
-  labs(x = "Depth", y = "Biodiversity observations") +
+  labs(x = "Depth (m)", y = "Biodiversity observations") +
   geom_vline(xintercept = 200, linetype = "dashed", color = "black") +
   geom_vline(xintercept = 1000, linetype = "dashed", color = "black") +
   geom_vline(xintercept = 4000, linetype = "dashed", color = "black") +
@@ -110,4 +111,29 @@ obis_clean |>
   geom_vline(xintercept = 200, linetype = "dashed", color = "black") +
   geom_vline(xintercept = 1000, linetype = "dashed", color = "black") +
   geom_vline(xintercept = 6000, linetype = "dashed", color = "black")
+
+##### nodes
+nodes_classification <- read_csv("nodes_obs_classified.csv") |>
+  dplyr::select(node_name, n, class)
+
+node_obs <- nodes_classification |>
+  group_by(class) |>
+  summarise(n = sum(n)) |>
+  arrange(-n) |>
+  mutate(all = sum(n)) |>
+  mutate(perc = n/all*100) |>
+  arrange(-perc)
+
+node_obs$class <- factor(
+  node_obs$class,
+  levels = c("Not classified", "Low income","Lower middle income","Upper middle income","High income"))
+
+ggplot(node_obs, aes(x = class, y = perc)) + geom_col() + coord_flip() +
+  ylab("Biodiversity observations\nmanaged (%)") +
+  xlab("") +
+  ylim(c(0,100)) +
+  theme_bw() +
+  theme(axis.text.y = element_text(angle = 45, hjust = 1))
+
+
 
